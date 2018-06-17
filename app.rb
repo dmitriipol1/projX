c5cbba0eba206ef781e65fe817d10ee4d34d3634
Dmitriy <dip@loud.su>
2018-06-17 19:03:36 +0300

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,10 +4,10 @@ require 'sinatra'
 require 'mysql2'
 
 client = Mysql2::Client.new(
-    :host => '85.93.128.108',
-    :username => 'puser',
-    :password => 'Puser12',
-    :database => 'projx',
+    :host => ' ',
+    :username => ' ',
+    :password => ' ',
+    :database => ' ',
     :encoding => 'utf8',
     :reconnect => true
 )
